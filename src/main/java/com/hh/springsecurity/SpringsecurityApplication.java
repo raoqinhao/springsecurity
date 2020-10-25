602ff24bd7c8853f70972d5feca42a2ad1ad1094
raoqinhao <976869901@qq.com>
2020-10-25 13:18:37 +0800

    解决项目url未找到问题

--- src/main/java/com/hh/springsecurity/SpringsecurityApplication.java
+++ src/main/java/com/hh/springsecurity/SpringsecurityApplication.java
@@ -3,8 +3,9 @@ package com.hh.springsecurity;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @MapperScan("com.hh.springsecurity.mapper")
 public class SpringsecurityApplication {
 
