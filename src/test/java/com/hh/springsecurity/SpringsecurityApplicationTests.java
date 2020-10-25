10cd8312cfe7cbb14b1ebc2d1b490a581c9e93f3
raoqinhao <976869901@qq.com>
2020-10-25 12:45:34 +0800

    install

--- src/test/java/com/hh/springsecurity/SpringsecurityApplicationTests.java
+++ src/test/java/com/hh/springsecurity/SpringsecurityApplicationTests.java
@@ -1,10 +1,10 @@
-package com.hh.springsecurity;
-
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpringsecurityApplicationTests {
-
-
-}
+//package com.hh.springsecurity;
+//
+//
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringsecurityApplicationTests {
+//
+//
+//}
