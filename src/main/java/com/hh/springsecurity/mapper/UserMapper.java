e9b9821becf07538deb5d5ded811b85fd76e241f
raoqinhao <976869901@qq.com>
2020-10-25 13:54:23 +0800

    将spring和mybatis的冲突解决

--- src/main/java/com/hh/springsecurity/mapper/UserMapper.java
+++ src/main/java/com/hh/springsecurity/mapper/UserMapper.java
@@ -4,8 +4,10 @@ import com.hh.springsecurity.pojo.UserBean;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
 @Mapper
+@Repository
 public interface UserMapper {
 
     @Select("SELECT * FROM u_user WHERE id = #{id}")
