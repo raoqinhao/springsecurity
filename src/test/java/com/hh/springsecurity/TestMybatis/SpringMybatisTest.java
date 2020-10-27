3e09cf17f0b7fe6763c3a0be160482bf506e86b9
raoqinhao <976869901@qq.com>
2020-10-27 21:11:51 +0800

    注释单元测试

--- src/test/java/com/hh/springsecurity/TestMybatis/SpringMybatisTest.java
+++ src/test/java/com/hh/springsecurity/TestMybatis/SpringMybatisTest.java
@@ -2,7 +2,6 @@ package com.hh.springsecurity.TestMybatis;
 
 import com.hh.springsecurity.mapper.UserMapper;
 import com.hh.springsecurity.pojo.UserBean;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -20,7 +19,7 @@ public class SpringMybatisTest {
     @Autowired
     private UserMapper userMapper;
 
-    @Test
+//    @Test
     public void getUserMapper() {
         UserBean userBean = userMapper.findUserById("");
         UserBean userName = userMapper.findUserByUserName("zhangsan");
@@ -29,14 +28,14 @@ public class SpringMybatisTest {
         System.out.println(userName.getUsername());
     }
 
-    @Test
+//    @Test
     public void insertUserInfo() {
         UserBean userBean = new UserBean(UUID.randomUUID().toString().replaceAll("-",""),"lisi","123","17167495555");
         int i = userMapper.saveUserByRegister(userBean);
         System.out.println(i);
     }
 
-    @Test
+//    @Test
     public void createUUID() {
         String uuid = UUID.randomUUID().toString().replaceAll("-", "");
         System.out.println(uuid);
