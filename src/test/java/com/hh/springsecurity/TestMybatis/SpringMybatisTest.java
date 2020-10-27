4e8747b0470ef15d85144461c2e733dc5548313a
raoqinhao <976869901@qq.com>
2020-10-27 14:37:22 +0800

    去除无用注解

--- src/test/java/com/hh/springsecurity/TestMybatis/SpringMybatisTest.java
+++ src/test/java/com/hh/springsecurity/TestMybatis/SpringMybatisTest.java
@@ -7,7 +7,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.UUID;
@@ -21,7 +20,6 @@ public class SpringMybatisTest {
     @Autowired
     private UserMapper userMapper;
 
-//    @Rollback(false)
     @Test
     public void getUserMapper() {
         UserBean userBean = userMapper.findUserById("");
@@ -32,7 +30,6 @@ public class SpringMybatisTest {
     }
 
     @Test
-//    @Rollback(false)
     public void insertUserInfo() {
         UserBean userBean = new UserBean(UUID.randomUUID().toString().replaceAll("-",""),"lisi","123","17167495555");
         int i = userMapper.saveUserByRegister(userBean);
