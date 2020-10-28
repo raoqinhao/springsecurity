70cd5e38bc853464f3b9d4f1875056fa4ced908c
raoqinhao <976869901@qq.com>
2020-10-28 22:10:17 +0800

    去除test

--- src/test/java/com/hh/springsecurity/TestMybatis/SpringMybatisTest.java
+++ src/test/java/com/hh/springsecurity/TestMybatis/SpringMybatisTest.java
@@ -2,12 +2,16 @@ package com.hh.springsecurity.TestMybatis;
 
 import com.hh.springsecurity.mapper.UserMapper;
 import com.hh.springsecurity.pojo.UserBean;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.rmi.NotBoundException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.UUID;
 
 
@@ -26,6 +30,17 @@ public class SpringMybatisTest {
         System.out.println(userBean);
         System.out.println(userName);
         System.out.println(userName.getUsername());
+
+    }
+
+//    @Test
+    public void testException () throws Exception {
+        String str = new String("ces");
+        System.out.println(str);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String format = sdf.format(new Date());
+        String s = String.format(format + "%b", "a");
+        throw new NotBoundException(s);
     }
 
     //    @Test
