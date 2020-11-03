4eb69c49dd8031fe74c4df63fb7e3fa661f087b2
raoqinhao <976869901@qq.com>
2020-11-03 09:16:43 +0800

    修改全局异常存放到controller位置中

--- src/main/java/com/hh/springsecurity/controller/GlobalExceptionHandlerController.java
+++ src/main/java/com/hh/springsecurity/controller/GlobalExceptionHandlerController.java
@@ -1,5 +1,6 @@
-package com.hh.springsecurity.exception;
+package com.hh.springsecurity.controller;
 
+import com.hh.springsecurity.exception.NotFoundUserNameException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,9 +8,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandlerController {
 
-    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandlerController.class);
 
     @ExceptionHandler(value = NotFoundUserNameException.class)
     @ResponseBody
