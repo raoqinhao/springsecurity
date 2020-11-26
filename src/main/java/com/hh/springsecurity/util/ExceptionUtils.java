c073b88db67577ae71ab59c8ab8cf4023e869906
raoqinhao <976869901@qq.com>
2020-11-26 08:29:23 +0800

    工具类添加提示信息。

--- src/main/java/com/hh/springsecurity/util/ExceptionUtils.java
+++ src/main/java/com/hh/springsecurity/util/ExceptionUtils.java
@@ -2,6 +2,12 @@ package com.hh.springsecurity.util;
 
 public class ExceptionUtils {
 
+    /**
+     * 异常信息统一返回
+     * @param code
+     * @param message
+     * @return
+     */
     public static String pushMessage(String code, String message) {
         return String.format("{\"code\":\"%s\",\"message\":\"%s\"}",code,message);
     }
