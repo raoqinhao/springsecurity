49345b0e782afb425d9774856b311e0a34c408f6
raoqinhao <976869901@qq.com>
2020-12-17 15:20:15 +0800

    整理代码

--- src/main/java/com/hh/springsecurity/service/UserDetailsServiceImpl.java
+++ src/main/java/com/hh/springsecurity/service/UserDetailsServiceImpl.java
@@ -1,19 +1,19 @@
 package com.hh.springsecurity.service;
 
-        import com.hh.springsecurity.mapper.UserMapper;
-        import com.hh.springsecurity.pojo.UserBean;
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.security.core.GrantedAuthority;
-        import org.springframework.security.core.authority.AuthorityUtils;
-        import org.springframework.security.core.userdetails.User;
-        import org.springframework.security.core.userdetails.UserDetails;
-        import org.springframework.security.core.userdetails.UserDetailsService;
-        import org.springframework.security.core.userdetails.UsernameNotFoundException;
-        import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-        import org.springframework.stereotype.Service;
+import com.hh.springsecurity.mapper.UserMapper;
+import com.hh.springsecurity.pojo.UserBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
-        import java.util.ArrayList;
-        import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service("userDetailsService")
 public class UserDetailsServiceImpl implements UserDetailsService {
